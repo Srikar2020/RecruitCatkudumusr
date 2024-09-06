afbbe7558f3182517989a85779e84127c253faeb
Srikar Kudumula <68241974+Srikar2020@users.noreply.github.com>
2024-09-05 20:45:11 -0700

    Assignment 2: Added Models Folder and Added Classes using the Class-builder that was provided.

---
+++ RecruitCatkudumusr/Models/Class.cs
@@ -0,0 +1,53 @@
+﻿namespace RecruitCatkudumusr.Models
+{
+	public class Candidate
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime? StartDate { get; set; }
+		public decimal TargetSalary { get; set; }
+		public int Id { get; set; }
+		public int? Age { get; set; }
+		public int? CompanyID { get; set; }
+		public int JobTitleID { get; set; }
+		public int IndustryID { get; set; }
+
+	}
+
+	public class Company
+	{
+		public string Name { get; set; }
+		public string PositionName { get; set; }
+		public DateTime? StartDate { get; set; }
+		public string Location { get; set; }
+		public decimal MinimumSalary { get; set; }
+		public decimal MaximumSalary { get; set; }
+		public int Id { get; set; }
+		public int CandidateID { get; set; }
+		public List<Candidate> Candidates { get; set; }
+		public int IndustryID { get; set; }
+		public int JobTitleID { get; set; }
+
+	}
+
+	public class JobTitle
+	{
+		public string Title { get; set; }
+		public decimal MinimumSalary { get; set; }
+		public decimal MaximumSalary { get; set; }
+		public int Id { get; set; }
+		public List<Candidate> Candidates { get; set; }
+
+	}
+
+	public class Industry
+	{
+		public string Name { get; set; }
+		public int Id { get; set; }
+		public DateTime StartDate { get; set; }
+		public List<Candidate> Candidates { get; set; }
+		public List<Company> Companies { get; set; }
+
+	}
+
+}
